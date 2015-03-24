88e454734c31b84b4d8026515b064189f6ddae8e
other <other@mewmew.cn>
2015-03-24 11:40:19 +0800

    add jx support for system , using jx runtime

--- bin/mewbot.sh
+++ bin/mewbot.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 PRG="$0"
 PRGDIR=`dirname "$PRG"`
+NODE=node
 PATH=/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PRGDIR/../node_modules/.bin:$PRGDIR/../node_modules/other-hubot/node_modules/.bin:$PATH
 
+which jx > /dev/null 2>&1
+if [ $? == 0 ]; then
+    NODE=jx
+fi
 
-
-node $PRGDIR/../node_modules/coffee-script/bin/coffee $PRGDIR/../core/startup.coffee $@
+$NODE $PRGDIR/../node_modules/coffee-script/bin/coffee $PRGDIR/../core/startup.coffee $@
