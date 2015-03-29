503d4bbe62d6f9df5736854d4518071e808e3e98
other <other@mewmew.cn>
2015-03-29 16:33:58 +0800

    forever

--- forever.js
+++ forever.js
@@ -1,1 +1,7 @@
-require('forever').cli.start();
+var path = require('path');
+var fs   = require('fs');
+var coffee = require('coffee-script')
+coffee.register();
+jxcore.monitor.followMe(function(err,txt){
+	require('./core/startup.coffee')
+});
